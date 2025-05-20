1d845bbc6a2508de4b15b583557b5949be2ef197
Ravou <oliviaravou@hotmail.fr>
2025-05-21 00:00:02 +0200

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Class that defines a square by its size."""
+
+    def __init__(self, size=0):
+        """Initialize the square with optional size (default is 0)."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
