adac3fcb1e713e29d1af0c275211c39936a32d83
Ravou <oliviaravou@hotmail.fr>
2025-05-20 11:30:58 +0200

    commit all task

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class Square that represents a square."""
+
+
+class Square:
+    """A class to represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square with a given size."""
+        self.__size = size  # private instance attribute
