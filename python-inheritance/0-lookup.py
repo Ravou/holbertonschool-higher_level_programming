320de4b52f99998e2e197a9e5ffdc7dd4d4e4e6e
Ravou <oliviaravou@hotmail.fr>
2025-05-29 17:24:19 +0200

    add docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Use this shebang to specify the Python interpreter for the script."""
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return dir(obj)
