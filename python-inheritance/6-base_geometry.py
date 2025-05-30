89ba0afb8825b522420f5bee4d354e05b57c04c3
Ravou <oliviaravou@hotmail.fr>
2025-05-30 15:49:36 +0200

    Task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Module that write a class """
 
-
-def area(self):
-    """ function that raise an Exception with the message """
-    except ValueError:
-        print ("area() is not implemented")
+class BaseGeometry():
+    """ empty class """
+    def area(self):
+        """ function that raise an Exception with the message """
+        raise Exception ("area() is not implemented")
