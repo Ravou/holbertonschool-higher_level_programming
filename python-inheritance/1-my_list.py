46742fe47d8fb05bd047d53fafd4dd7dc138adf2
Ravou <oliviaravou@hotmail.fr>
2025-05-30 10:32:56 +0200

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/!
+""" Use the program for execute for this script """
+class MyList(list):
+    """ Custom list class that inherits from the built-in list. """
+    def print_sorted(self):
+        """Prints the list in acsending order (without modifying the original list). """
+        print(sorted(self))
