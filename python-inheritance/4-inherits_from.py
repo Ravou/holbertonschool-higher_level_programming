863db603903bb3740353f39ecda447dc9443b8df
Ravou <oliviaravou@hotmail.fr>
2025-05-30 14:21:52 +0200

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Use this for execute the function """
+
 def inherits_from(obj, a_class):
     """ check if the object is an instance or not """
-    return (obj, a_class):
+    return (obj, a_class) and type(obj) is not a_class
