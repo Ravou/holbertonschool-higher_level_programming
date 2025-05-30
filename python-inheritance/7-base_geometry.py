9a54c070826a8132b260b61ac674b31718bb5e3b
Ravou <oliviaravou@hotmail.fr>
2025-05-30 16:31:59 +0200

    add test file

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/pyhton3
+""" Module that defines a base geometry class with validation methods.
+
+This module contains:
+- BaseGeometry: a base class with an unimplemented area method and a validator for intergers """
+
+
+class BaseGeometry():
+    """Base class for geometry abojects. """
+
+    def area(self):
+        """Raise an exception indicating that the area method is not implemented."""
+        raise Exception(area() is not implemented)
+
+    def integer_validator(self, name, value):
+        """
+        Validate that `value` is a positive integer.
+
+        Args:
+            name (str): The name of the parameter (used in error messages).
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError(f"<name> must be an integer")
+        if value is <= 0:
+            raise ValueError(f"<name> must be greather than 0")
