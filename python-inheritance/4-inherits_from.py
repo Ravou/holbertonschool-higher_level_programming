08143b5addc929781f6d185f30e6a1018f34fc7e
Ravou <oliviaravou@hotmail.fr>
2025-06-01 16:22:14 +0200

    add space

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a function to check if an object inherits from a class."""
 
+
 def inherits_from(obj, a_class):
     """
     Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
