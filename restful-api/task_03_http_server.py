dc9049c14371be22f7d4f7dadaac65207a4989e3
Ravou <oliviaravou@hotmail.fr>
2025-06-13 12:02:13 +0200

    add

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -49,8 +49,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(message).encode("utf-8"))
+            self.wfile.write(b'{"error": "Endpoint not found"}')
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
