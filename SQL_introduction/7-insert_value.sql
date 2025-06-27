61c2f891074b5abe24eb3b245eefb3674115be4f
Ravou <oliviaravou@hotmail.fr>
2025-06-27 08:38:54 +0200

    fix

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- This script add a new row in the table
-SELECT
-ROW_NUMBER() OVER () id = 89, name = Best_school FROM first_table;
+INSERt INTO first_table (id, name)
+VALUES (89, 'Best_school');
