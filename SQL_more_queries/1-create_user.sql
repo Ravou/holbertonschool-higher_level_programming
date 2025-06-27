c61d9092bfb7a96d0cb9842cc784636957d2b86d
Ravou <oliviaravou@hotmail.fr>
2025-06-27 13:43:10 +0200

    add WITH

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- This script create server user 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENYIFIED BY 'password'; 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENYIFIED WITH authentication_plugin BY 'password'; 
