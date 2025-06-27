d21a6190c34415c80511ee487954e63b4a27898b
Ravou <oliviaravou@hotmail.fr>
2025-06-27 14:51:33 +0200

    fix

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- This script create database and user 
 SELECT
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
-CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT PRIVILEGE ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
