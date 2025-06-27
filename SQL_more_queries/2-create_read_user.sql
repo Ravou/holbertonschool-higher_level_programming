e32edc151be5c2e7387f165078ef58b69af8177e
Ravou <oliviaravou@hotmail.fr>
2025-06-27 15:29:45 +0200

    add usage

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- This script create database and user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
