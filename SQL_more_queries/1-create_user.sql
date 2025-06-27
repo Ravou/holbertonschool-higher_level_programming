26f532d11df70ec3155ee3cb0f792fb4ce838b51
Ravou <oliviaravou@hotmail.fr>
2025-06-27 13:52:12 +0200

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
--- This script create server user 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENYIFIED WITH authentication_plugin BY 'password'; 
+-- This script create server user
+INSERT INTO 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENYIFIED WITH authentication_plugin BY 'password';
+GRANT PRIVILEGE ON database.table TO 'username'@'host';
