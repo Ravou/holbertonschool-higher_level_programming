1fbaa1be21753092076d9a66107736d516752ef9
Ravou <oliviaravou@hotmail.fr>
2025-06-27 14:05:15 +0200

    add WITH OPTION

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- This script create server user
-INSERT INTO 
+-- and grants all privileges on the server
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
